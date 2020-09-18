2cecabda313eb854dc754ed10ddbff3bc9bebd17
Jacob Kagon <jacob@kagon.net>
2020-09-18 21:47:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,10 +7,9 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, value|
     if value < min_value
       min_value = value
-      
+      key_value = name
     end
-    key_value = name
   end
 
- puts key_value
+ key_value
 end
